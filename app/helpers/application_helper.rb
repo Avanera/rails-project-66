b8f16c5fe9270711b87e70f7758e171f0518484b
Arina Zaitseva <avanero@bk.ru>
2024-09-24 18:18:39 -0400

    add layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def get_flash_class_for(type)
+    {
+      success: 'alert alert-success',
+      error: 'alert alert-danger',
+      alert: 'alert alert-warning',
+      notice: 'alert alert-info'
+    }[type.to_sym]
+  end
 end
