25d438c2cda4a64e326835ed7e6b50ab77d1b358
Arina Zaitseva <avanero@bk.ru>
2024-09-25 18:45:01 -0400

    create repositories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_24_233601) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_25_153707) do
+  create_table "repositories", force: :cascade do |t|
+    t.string "name"
+    t.integer "github_id"
+    t.string "full_name"
+    t.string "language"
+    t.string "clone_url"
+    t.string "ssh_url"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_repositories_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "name"
@@ -21,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_24_233601) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "repositories", "users"
 end
