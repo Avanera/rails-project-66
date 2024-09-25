e3fd14df114b8742550c85b76fe148f066d36bbe
Arina Zaitseva <avanero@bk.ru>
2024-09-24 21:55:37 -0400

    implement authentication using omniauth

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+OmniAuth.config.test_mode = true
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
@@ -13,5 +15,35 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+  end
+end
+
+module ActionDispatch
+  class IntegrationTest
+    def sign_in(user, _options = {}) # rubocop:disable Metrics/MethodLength
+      auth_hash = {
+        provider: 'github',
+        uid: '12345',
+        info: {
+          email: user&.email || 'john@example.com',
+          name: user&.name || 'john',
+          nickname: user&.nickname || 'john',
+          image_url: user&.image_url || 'example.com'
+        },
+        credentials: {
+          token: user&.token || 'ghp_23iac7xzd0t95p496ir4h5j11r73qomtpkzv'
+        }
+      }
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash::InfoHash.new(auth_hash)
+      get callback_auth_url('github')
+    end
+
+    def signed_in?
+      session[:user_id].present? && current_user.present?
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 end
