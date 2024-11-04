a831ae8950c7dcd089c3c490b2422ad9f29ebb32
Arina Zaitseva <avanero@bk.ru>
2024-11-04 18:21:55 -0300

    create javascript_linter and separate it from ruby_linter

--- app/services/check_repository_service.rb
+++ app/services/check_repository_service.rb
@@ -24,14 +24,18 @@ class CheckRepositoryService
     clone_repo_to_tmp
     assign_check_commit_id
     @check.start!
-    run_rubocop
+    run_linter
     @check.finish!
+  end
+
+  def run_linter
+    linter = "Linter::#{@repository.language.capitalize}Linter".constantize
+    linter.new(@repo_path, @repository, @check).run
   end
 
   def clone_repo_to_tmp
     remove_tmp_directory if Dir.exist?(@repo_path)
     command_str = "git clone #{@repository.clone_url} #{@repo_path}"
-
     Open3.popen3(command_str) do |_, stdout, stderr, wait_thr|
       Rails.logger.info("Starting git clone from #{@repository.clone_url} to #{@repo_path}")
       Rails.logger.info(stdout.read)
@@ -63,38 +67,6 @@ class CheckRepositoryService
     @check.fail!
     Rails.logger.error(t('services.check_repo.errors.failed_to_get_commit_id',
                          stderr: stderr.strip))
-  end
-
-  def run_rubocop
-    stdout, = Open3.capture3("rubocop --config=.rubocop.yml --format json #{@repo_path}")
-    offenses = parse_rubocop_offenses(stdout)
-
-    @check.update(passed: offenses.empty?, result: offenses.to_json)
-  end
-
-  def parse_rubocop_offenses(stdout)
-    JSON.parse(stdout)['files'].flat_map do |file|
-      file_path = file['path'].sub(@repo_path, '')
-      file['offenses'].map do |offense|
-        format_offense(file_path, offense)
-      end
-    end
-  end
-
-  def format_offense(file_path, offense)
-    location = offense['location']
-    {
-      path: file_path,
-      message: offense['message'],
-      cop_name: offense['cop_name'],
-      line_and_column: "#{location['start_line']}:#{location['start_column']}",
-      file_link: generate_file_link(file_path, location)
-    }
-  end
-
-  def generate_file_link(file_path, location)
-    base_url = @repository.clone_url.gsub('.git', '')
-    "#{base_url}/tree/#{@check.commit_id}/#{file_path}#L#{location['start_line']}"
   end
 
   def handle_failure(error)
