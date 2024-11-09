afdf182b92afb1341681b12daf69c34cf0e64976
Arina Zaitseva <avanero@bk.ru>
2024-11-08 21:09:15 -0300

    do not verify the signature if it was not sent

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -2,7 +2,7 @@
 
 class Api::ChecksController < Api::ApplicationController
   def create
-    return head :unauthorized unless verify_signature
+    return head :unauthorized unless verify_signature_if_any
     return head :unprocessable_entity unless valid_github_event?
 
     repository = Repository.find_by(github_id: params.dig(:repository, :id))
@@ -18,7 +18,9 @@ class Api::ChecksController < Api::ApplicationController
 
   private
 
-  def verify_signature
+  def verify_signature_if_any
+    return true unless request.env['HTTP_X_HUB_SIGNATURE_256']
+
     secret = ENV.fetch('WEBHOOK_SECRET_GITHUB')
     digest = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), secret, request.raw_post)
     signature = "sha256=#{digest}"
